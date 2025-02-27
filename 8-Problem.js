ff90bc945d61a050ca2b38505152359aefec3516
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-27 12:00:50 +0530

    feat: add solution for 8-Problem

---
+++ 8-Problem.js
@@ -0,0 +1,7 @@
+// 8-Problem statement - Declare a variable without assigning a value. Log its type.
+
+let variableWithoutAssignment;
+
+console.log(
+	`Type of a variable without assignment is: ${typeof variableWithoutAssignment}`
+);
