b9e2b8faab32c21a26bb05f339677c1e819587ba
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-27 11:33:53 +0530

    refactor(2-Problem.js): improve by using Math.PI for higher precision and accuracy

--- 2-Problem.js
+++ 2-Problem.js
@@ -1,5 +1,9 @@
 // 2-Problem statement - Create a constant to store the value of PI and log it.
 
-const PI = 3.142;
+// assigning hard coded manually value of PI
+// const PI = 3.142;
+
+// use built in method for higher precision
+const PI = Math.PI;
 
 console.log(`Value of PI is: ${PI}`);
