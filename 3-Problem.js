34bc0bd743ff9976c2205603ed063f814386d232
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-27 11:44:59 +0530

    feat: add solution for 3-Problem

---
+++ 3-Problem.js
@@ -0,0 +1,11 @@
+// 3-Problem statement - Reassign a value to a variable declared with let and log the result.
+
+let status = "JavaScript Noob";
+
+console.log("====== Value Before Reassign ======");
+console.log(status);
+
+status = "JavaScript Pro";
+
+console.log("\n====== Value After Reassign ======");
+console.log(status);
