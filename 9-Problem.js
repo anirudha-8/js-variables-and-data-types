74d92dfbf61c71aecc449d30daaecaac95e9dae9
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-27 12:03:34 +0530

    feat: add solution for 9-Problem

---
+++ 9-Problem.js
@@ -0,0 +1,5 @@
+// 9-Problem statement - Create a variable with undefined and log its type.
+
+let undefinedVariable = undefined;
+
+console.log(`The type of ${undefinedVariable} is: ${typeof undefinedVariable}`);
