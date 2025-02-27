6bbd125e562413b78fe5520001ccdfa9babd1c3c
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-27 11:57:06 +0530

    feat: add solution for 7-Problem

---
+++ 7-Problem.js
@@ -0,0 +1,13 @@
+// 7-Problem statement - Create three variables of types string, number, and boolean, and log their values.
+
+const fullName = "Anirudha Bele";
+
+const age = 23;
+
+const isMarried = false;
+
+console.log(`Type of "string" variable: ${typeof fullName}`);
+
+console.log(`Type of "number" variable: ${typeof age}`);
+
+console.log(`Type of "boolean" variable: ${typeof isMarried}`);
