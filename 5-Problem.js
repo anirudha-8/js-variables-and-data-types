c52efc6aeb648790edc0402104a48fb142342db6
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-27 11:52:02 +0530

    feat: add solution for 5-Problem

---
+++ 5-Problem.js
@@ -0,0 +1,5 @@
+// 5-Problem statement - Create a variable with a number as a string (e.g., "25") and log its type.
+
+const number = "25";
+
+console.log(`Type of variable "number" is: ${typeof number}`);
